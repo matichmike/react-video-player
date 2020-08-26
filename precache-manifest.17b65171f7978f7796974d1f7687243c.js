c1b51f576f3f745d7b8edb55559c3ce571bb6fda
Michael Matich <matich.mike@gmail.com>
2020-08-26 19:55:55 -0400

    Updates

---
+++ precache-manifest.17b65171f7978f7796974d1f7687243c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5bdd4a6f0adf2437459ea0af08ec457",
+    "url": "/react-video-player/index.html"
+  },
+  {
+    "revision": "32f6768dcc5f8a260b0e",
+    "url": "/react-video-player/static/css/main.4d181be5.chunk.css"
+  },
+  {
+    "revision": "1be8eefbbebb5071e027",
+    "url": "/react-video-player/static/js/2.2b4a1d0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-video-player/static/js/2.2b4a1d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32f6768dcc5f8a260b0e",
+    "url": "/react-video-player/static/js/main.26bd5db8.chunk.js"
+  },
+  {
+    "revision": "9d6b921c0d1d98e26a0d",
+    "url": "/react-video-player/static/js/runtime-main.1cdda6bb.js"
+  }
+]);
