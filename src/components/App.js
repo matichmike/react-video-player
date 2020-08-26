12a2a0f4aebab10cc3a53abb5d199ba89fe49912
Michael Matich <matich.mike@gmail.com>
2020-08-26 19:38:27 -0400

    default search value set

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,20 @@ class App extends React.Component {
     selectedVideo: null
   };
 
+  componentDidMount() {
+    this.onTermSubmit('Japan sakura blossom');
+  }
+
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
       params: {
         q: term
       }
     })
-    this.setState({ videos: response.data.items });
+    this.setState({ 
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = video => {
@@ -28,11 +35,19 @@ class App extends React.Component {
       <SearchBar 
       onFormSubmit={this.onTermSubmit} 
       />
-      <VideoDetail video={this.state.selectedVideo} />
-      <VideoList 
-      onVideoSelect={this.onVideoSelect} 
-      videos={this.state.videos}
-      />
+      <div className="ui grid">
+            <div className="ui row">
+              <div className="eleven wide column">
+          <VideoDetail video={this.state.selectedVideo} />
+          </div>
+          <div className="five wide column">
+          <VideoList 
+          onVideoSelect={this.onVideoSelect} 
+          videos={this.state.videos}
+          />
+          </div>
+          </div>
+        </div>
       </div>
   }
 }
